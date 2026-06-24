897180b1416992cc78508f42623f0375af48ec82
Rubem Puttini <rubem.staciariniputtini@gmail.com>
2026-06-24 20:10:44 +0100

    fix: update cache control

--- api/geo.js
+++ api/geo.js
@@ -5,5 +5,6 @@ export default function handler(req, res) {
     'BR';
 
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-store');
   res.status(200).json({ country });
 }
